dd201fade48c615540dad798da22592561348753
MayThura <maythuralwin487@gmail.com>
2021-05-02 16:18:11 +0900

    Initialize server side
    Initialize Koa server for STL file upload and download

---
+++ file-server/server.js
@@ -0,0 +1,43 @@
+const koa = require('koa');
+const koaBody = require('koa-body');
+const cors = require('@koa/cors');
+const Router = require('@koa/router');
+const send = require('koa-send');
+const path = require('path');
+
+const app = new koa();
+
+const router = new Router();
+
+const dataPath = '/stl-viewer/public';
+
+app.use(cors());
+
+app.use(koaBody({
+  multipart: true,
+  formidable: {
+    uploadDir: `${process.env.HOME}${dataPath}`,
+    keepExtensions: true,
+  },
+  urlencoded: true,
+}));
+
+
+router.post('/upload', async (ctx) => { 
+    ctx.body = ctx.request.files.myFile.path;
+    return ctx;
+});
+
+router.get('/download/:name', async (ctx) => {
+    const name = ctx.params.name;
+    const filePath = dataPath + '/' + name;
+    ctx.attachment(filePath);
+    ctx.response.status = 200;
+    await send(ctx, filePath, { root: process.env.HOME });
+});
+
+app.use(router.routes());
+
+app.listen(3001, () => {
+  console.log('koa is listening in 3001');
+})
